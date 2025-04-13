bf77210f68f17a76fd2165f4194ae19862cb4a35
Carmen Jara Alvarez <carjaralv@alum.us.es>
2025-04-13 20:04:33 +0200

    sidebar routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,6 +16,9 @@ import { DuaComponent as Unit2DuaComponent } from './body/unit2/dua/dua.componen
 import { DescriptionComponent as Unit2DescriptionComponent } from './body/unit2/description/description.component';
 import { SpecificationComponent as Unit2SpecificationComponent } from './body/unit2/specification/specification.component';
 import { SequencingComponent as Unit2SequencingComponent } from './body/unit2/sequencing/sequencing/sequencing.component';
+import { Phase1Component as Unit2Phase1Component } from './body/unit2/sequencing/phase1/phase1.component';
+import { Phase2Component as Unit2Phase2Component } from './body/unit2/sequencing/phase2/phase2.component';
+import { Phase3Component as Unit2Phase3Component } from './body/unit2/sequencing/phase3/phase3.component';
 import { EvaluationComponent as Unit2EvaluationComponent } from './body/unit2/evaluation/evaluation.component';
 import { IdentificationComponent as Unit3IdentificationComponent } from './body/unit3/identification/identification.component';
 import { JustificationComponent as Unit3JustificationComponent } from './body/unit3/justification/justification.component';
@@ -23,6 +26,9 @@ import { DuaComponent as Unit3DuaComponent } from './body/unit3/dua/dua.componen
 import { DescriptionComponent as Unit3DescriptionComponent } from './body/unit3/description/description.component';
 import { SpecificationComponent as Unit3SpecificationComponent } from './body/unit3/specification/specification.component';
 import { SequencingComponent as Unit3SequencingComponent } from './body/unit3/sequencing/sequencing/sequencing.component';
+import { Phase1Component as Unit3Phase1Component } from './body/unit3/sequencing/phase1/phase1.component';
+import { Phase2Component as Unit3Phase2Component } from './body/unit3/sequencing/phase2/phase2.component';
+import { Phase3Component as Unit3Phase3Component } from './body/unit3/sequencing/phase3/phase3.component';
 import { EvaluationComponent as Unit3EvaluationComponent } from './body/unit3/evaluation/evaluation.component';
 
 export const routes: Routes = [
@@ -54,6 +60,9 @@ export const routes: Routes = [
       { path: 'descripcion', component: Unit2DescriptionComponent },
       { path: 'concrecion', component: Unit2SpecificationComponent },
       { path: 'secuenciacion', component: Unit2SequencingComponent },
+      { path: 'secuenciacion/faseinicial', component: Unit2Phase1Component },
+      { path: 'secuenciacion/fasedesarrollo', component: Unit2Phase2Component },
+      { path: 'secuenciacion/fasesintesis', component: Unit2Phase3Component },
       { path: 'evaluacion', component: Unit2EvaluationComponent }
     ]
   },
@@ -67,6 +76,9 @@ export const routes: Routes = [
       { path: 'descripcion', component: Unit3DescriptionComponent },
       { path: 'concrecion', component: Unit3SpecificationComponent },
       { path: 'secuenciacion', component: Unit3SequencingComponent },
+      { path: 'secuenciacion/faseinicial', component: Unit3Phase1Component },
+      { path: 'secuenciacion/fasedesarrollo', component: Unit3Phase2Component },
+      { path: 'secuenciacion/fasesintesis', component: Unit3Phase3Component },
       { path: 'evaluacion', component: Unit3EvaluationComponent }
     ]
   }
